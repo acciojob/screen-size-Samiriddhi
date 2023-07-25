5c25c3cc206e56df2941199dbd4e826d243f9fb7
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-07-25 21:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+
+// Function to update the width and height on the page
+    function updateWidthAndHeight() {
+      const sizeText = document.getElementById('sizeText');
+
+      // Get the updated width and height of the window
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      // Update the <h1> tag with the new values
+      sizeText.textContent = `Width: ${width} and Height: ${height}`;
+    }
+
+    // Call the function initially to display the initial width and height
+    updateWidthAndHeight();
+
+    // Add an event listener to the window's 'resize' event
+    window.addEventListener('resize', updateWidthAndHeight);
